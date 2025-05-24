b361ef90afebc52a18e25e0ada1d3993939a0502
Jack Powell <jackjpowell@gmail.com>
2025-05-24 10:20:10 -0400

    Finish discovery support

--- intg-samsungtv/discover.py
+++ intg-samsungtv/discover.py
@@ -1,24 +1,37 @@
 """Discovery module for SDDP protocol."""
 
-from typing import Optional, Iterable
+from dataclasses import dataclass
+from typing import Iterable, Optional
+
 import sddp_discovery_protocol as sddp
-from sddp_discovery_protocol.constants import SDDP_MULTICAST_ADDRESS, SDDP_PORT
 from sddp_discovery_protocol.client import DEFAULT_RESPONSE_WAIT_TIME
+from sddp_discovery_protocol.constants import SDDP_MULTICAST_ADDRESS, SDDP_PORT
 
 
+@dataclass
 class SddpResponseInfo:
     """
     This class is used to store information about a response received from the SDDP protocol.
     It contains the datagram and the address of the sender.
     """
 
-    def __init__(self):
-        self.datagram = None
-        self.address = None
+    def __init__(self, address=None, tv_type=None):
+        self.address = address
+        self.type = tv_type
 
     def __repr__(self):
-        return f"SddpResponseInfo(datagram={self.datagram}, address={self.address})"
+        return f"SddpResponseInfo(type={self.type}, address={self.address})"
 
+
+class SddpDiscovery:
+    """
+    This class is used to store information about a response received from the SDDP protocol.
+    It contains the datagram and the address of the sender.
+    """
+
+    def __init__(self):
+        self.datagrams: list = []
+        self.discovered: list[SddpResponseInfo] = []
 
     async def get(
         self,
@@ -38,7 +51,13 @@ class SddpResponseInfo:
             bind_addresses=bind_addresses,
             include_loopback=include_loopback,
         ) as client:
-            async with client.search() as search_request:
+            async with client.search(
+                search_pattern=search_pattern, response_wait_time=response_wait_time
+            ) as search_request:
                 async for response_info in search_request.iter_responses():
-                    self.datagram = response_info.datagram
-                    self.address = response_info.src_addr
+                    info = SddpResponseInfo(
+                        response_info.datagram.hdr_from[0],
+                        response_info.datagram.hdr_type,
+                    )
+                    self.datagrams.append(response_info.datagram)
+                    self.discovered.append(info)
